45e27bbf22a28b509eb02c860c47f49ea9f5ab7b
stevenna <theyoung2002@naver.com>
2022-07-02 17:43:05 +0900

    solve first assignment

---
+++ starter/terraform/_config.tf
@@ -0,0 +1,15 @@
+terraform {
+   backend "s3" {
+     bucket = "theyoung-sre-course2-terraform"
+     key    = "terraform/terraform.tfstate"
+     region = "us-east-2"
+   }
+ }
+
+ provider "aws" {
+   region = "us-east-2"
+
+   default_tags {
+     tags = local.tags
+   }
+ }
